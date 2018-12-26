c17dc85e7acad474639ec28f2769f80d9afc74f2
Olex Lozitskiy <olex.developer@gmail.com>
2018-12-25 17:46:06 -0800

    Chapter 14. Starting Map System Component

--- dev/MyProject/Gem/Code/Source/MyProjectModule.cpp
+++ dev/MyProject/Gem/Code/Source/MyProjectModule.cpp
@@ -7,6 +7,7 @@
 #include <MyComponent.h>
 #include <OscillatorComponent.h>
 #include <MySpawnerComponent.h>
+#include <StartingMapSystemComponent.h>
 
 namespace MyProject
 {
@@ -26,6 +27,7 @@ namespace MyProject
                 MyComponent::CreateDescriptor(),
                 OscillatorComponent::CreateDescriptor(),
                 MySpawnerComponent::CreateDescriptor(),
+                StartingMapSystemComponent::CreateDescriptor(),
             });
         }
 
@@ -36,6 +38,7 @@ namespace MyProject
         {
             return AZ::ComponentTypeList{
                 azrtti_typeid<MyProjectSystemComponent>(),
+                azrtti_typeid<StartingMapSystemComponent>(),
             };
         }
     };
