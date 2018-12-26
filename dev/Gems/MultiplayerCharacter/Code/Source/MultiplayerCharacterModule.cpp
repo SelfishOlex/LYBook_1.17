056e7919cf546636c5ce68a4ad8c2a8b1d512b53
Olex Lozitskiy <olex.developer@gmail.com>
2018-12-25 22:58:52 -0800

    Chapter 21. Client-Authoritative Player

---
+++ dev/Gems/MultiplayerCharacter/Code/Source/MultiplayerCharacterModule.cpp
@@ -0,0 +1,47 @@
+
+
+#include <AzCore/Memory/SystemAllocator.h>
+#include <AzCore/Module/Module.h>
+
+#include <MultiplayerCharacterSystemComponent.h>
+#include <InputCaptureComponent.h>
+#include <CameraControlsComponent.h>
+#include <PlayerControlsComponent.h>
+
+namespace MultiplayerCharacter
+{
+    class MultiplayerCharacterModule
+        : public AZ::Module
+    {
+    public:
+        AZ_RTTI(MultiplayerCharacterModule, "{4F62B4A7-8B93-4582-8179-E9B81DFB2467}", AZ::Module);
+        AZ_CLASS_ALLOCATOR(MultiplayerCharacterModule, AZ::SystemAllocator, 0);
+
+        MultiplayerCharacterModule()
+            : AZ::Module()
+        {
+            // Push results of [MyComponent]::CreateDescriptor() into m_descriptors here.
+            m_descriptors.insert(m_descriptors.end(), {
+                MultiplayerCharacterSystemComponent::CreateDescriptor(),
+                InputCaptureComponent::CreateDescriptor(),
+                CameraControlsComponent::CreateDescriptor(),
+                PlayerControlsComponent::CreateDescriptor(),
+            });
+        }
+
+        /**
+         * Add required SystemComponents to the SystemEntity.
+         */
+        AZ::ComponentTypeList GetRequiredSystemComponents() const override
+        {
+            return AZ::ComponentTypeList{
+                azrtti_typeid<MultiplayerCharacterSystemComponent>(),
+            };
+        }
+    };
+}
+
+// DO NOT MODIFY THIS LINE UNLESS YOU RENAME THE GEM
+// The first parameter should be GemName_GemIdLower
+// The second should be the fully qualified name of the class above
+AZ_DECLARE_MODULE_CLASS(MultiplayerCharacter_ef30c7d985bd4150b75452a44b4744ca, MultiplayerCharacter::MultiplayerCharacterModule)
